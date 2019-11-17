cecf7a29cffd8a21d76f37eb3c934ef1c72b8526
Baptiste Pillot <baptiste@pillot.fr>
2019-11-17 22:02:24 +0100

    CNF Update file watcher for your programming environment

---
+++ bin/update.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Configure your IDE to launch this batch each time a .php / .html file is modified
+rem - the working directory must be your project root directory
+rem - do not watch cache directory or it will update each time you focus your IDE
+
+echo.>update
